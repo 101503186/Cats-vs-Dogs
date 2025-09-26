cd263d339157c21fefd4b30e3cb44a41982c4563
SenkoSlab <148092513+101503186@users.noreply.github.com>
2025-09-26 15:18:22 -0400

    texture added to ground and player movement created

--- Cats vs Dogs/Assets/Scripts/PlayerMovement.cs
+++ Cats vs Dogs/Assets/Scripts/PlayerMovement.cs
@@ -2,6 +2,13 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    [SerializeField]
+    public float moveSpeed;
+
+    Vector2 movement;
+
+    public Rigidbody2D rb;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -11,6 +18,12 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+    }
+
+    private void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
     }
 }
