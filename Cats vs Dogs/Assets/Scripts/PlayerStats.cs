389f281b7c884db23c72a1bed4f4fbd87dfbb53d
SenkoSlab <148092513+101503186@users.noreply.github.com>
2025-12-03 22:16:03 -0500

    refactored player stats and migrated them to new script

---
+++ Cats vs Dogs/Assets/Scripts/PlayerStats.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+
+public class PlayerStats : MonoBehaviour
+{
+    PlayerMovement movement;
+    PlayerCombat combat;
+
+    void Awake()
+    {
+        movement = GetComponent<PlayerMovement>();
+        combat = GetComponent<PlayerCombat>();
+    }
+
+    public float MoveSpeed
+    {
+        get => movement.MoveSpeed;
+        set => movement.MoveSpeed = value;
+    }
+
+    public float ShootCooldown
+    {
+        get => combat.ShootCooldown;
+        set => combat.ShootCooldown = value;
+    }
+
+    public float BulletSpeed
+    {
+        get => combat.BulletSpeed;
+        set => combat.BulletSpeed = value;
+    }
+
+    public float BulletSize
+    {
+        get => combat.BulletSize;
+        set => combat.BulletSize = value;
+    }
+}
