42738688002c8be3bf0d1652ff3ebd611d8074dc
SenkoSlab <148092513+101503186@users.noreply.github.com>
2025-12-04 01:40:00 -0500

    player flipping fixed

--- Cats vs Dogs/Assets/Scripts/PlayerLook.cs
+++ Cats vs Dogs/Assets/Scripts/PlayerLook.cs
@@ -10,5 +10,14 @@ public class PlayerLook : MonoBehaviour
         Vector2 direction = (mousePos - transform.position);
         float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
         transform.rotation = Quaternion.Euler(0, 0, angle);
+
+        if (angle > 90f || angle < -90f)
+        {
+            transform.localScale = new Vector3(1, -1, 1);   // flipped
+        }
+        else
+        {
+            transform.localScale = new Vector3(1, 1, 1);    // normal
+        }
     }
 }
